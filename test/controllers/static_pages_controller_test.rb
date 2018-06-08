36d78a9317f28f80887b586822eadde734e3081e
Sharique <md.shahab@payu.in>
2018-06-08 16:11:59 +0530

    adding user tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,33 +7,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get route" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
 
   test "should get home" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "#{@base_title}"
-  end
-
-
-  test "should get contact" do
-    get static_pages_contact_url
+    get about_url
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
